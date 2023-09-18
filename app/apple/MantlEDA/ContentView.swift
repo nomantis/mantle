2bf6981bcbf8ebd29beee17ed72dd784e09c113b
nomantis <knightnomantis@gmail.com>
2023-09-18 19:54:00 +0100

    Add Xcode project with initial SwiftUI app

---
+++ app/apple/MantlEDA/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import OSLog
+import Swia
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
